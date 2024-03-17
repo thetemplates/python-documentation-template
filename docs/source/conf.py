f789abadb982bb61dc6c245f8e2feb31657448f7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-17 03:03:48 +0000

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,119 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+This is conf.py
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+Configuration file for the Sphinx documentation builder.
 
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html.
+
+Project information
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+import sphinx_rtd_theme
+import sphinx_design
+
+
+'''
+Basic
+'''
 project = 'An Illustrative Sphinx Template'
 copyright = '2024, Artificial Intelligence Unit'
 author = 'Artificial Intelligence Unit'
 release = '0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+'''
+General configuration
+https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+https://myst-parser.readthedocs.io/en/v0.15.1/sphinx/intro.html#install-a-new-sphinx-extension-and-use-its-functionality
+'''
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.githubpages',
+    'sphinx_design',
+    'myst_parser',
+    'sphinxcontrib.mermaid'
+]
 
+myst_enable_extensions = [
+    'amsmath',
+    'attrs_inline',
+    'colon_fence',
+    'deflist',
+    'dollarmath',
+    'fieldlist',
+    'html_admonition',
+    'html_image',
+    'replacements',
+    'smartquotes',
+    'strikethrough',
+    'substitution',
+    'tasklist',
+]
+
+
+'''
+https://myst-parser.readthedocs.io/en/latest/configuration.html
+'''
+myst_heading_anchors = 5
+
+
+'''
+Paths that contain templates, relative to this directory.
+'''
 templates_path = ['_templates']
+
+
+'''
+List of patterns, relative to source directory, that match files and
+directories to ignore when looking for source files.
+This pattern also affects html_static_path and html_extra_path.
+'''
 exclude_patterns = []
 
 
+'''
+Options for HTML output
+https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+'''
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_book_theme'
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+html_css_files = ['css/generic.css', 'css/figures.css']
+
+html_js_files = ['https://code.jquery.com/jquery-3.7.0.min.js',
+                 'https://code.highcharts.com/stock/highstock.js',
+                 'https://code.highcharts.com/stock/modules/data.js',
+                 'https://code.highcharts.com/stock/modules/exporting.js',
+                 'https://code.highcharts.com/stock/modules/export-data.js',
+                 'https://code.highcharts.com/stock/modules/accessibility.js',
+                 'https://code.highcharts.com/highcharts.js',
+                 'https://code.highcharts.com/modules/networkgraph.js',
+                 'https://viewer.diagrams.net/js/viewer-static.min.js',
+                 'js/latex.js']
+
+html_theme_options = {
+    'home_page_in_toc': True,
+    'show_navbar_depth': 2,
+    'max_navbar_depth': 4,
+    'collapse_navbar': True,
+    'navigation_with_keys': False,
+    'use_sidenotes': True
+}
+
+html_logo = ''
+
+html_context = {
+    'landing_page': {
+        'menu': [
+            {'title': 'Introduction',
+             'url': ''},
+            {'title': 'Data',
+             'url': ''},
+            {'title': 'Search',
+             'url': 'search.html'}
+        ]
+    }
+}
